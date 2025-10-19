c2985501ef924109143268a74880b818479e11e0
lucas <lsantos2003silva@gmail.com>
2025-10-19 15:30:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import json
-from pprint import pprint
-
 import requests
-from telegram import Update
-from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes,
-                          Updater)
-
-TOKEN = '7977384112:AAHuuTeQe2S8yz3IbPq7Mt6BVpc8LB5w27w'
-MESSAGE_FILE = '/tmp/test.txt'
-REMOTE_FILE = '/tmp/remotefile.json'
-MAGICMIRROR_URL = 'http://localhost:8080/custom-message'
-
 import logging
+from pprint import pprint
 from logging import Logger
+from telegram import Update
+from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes, Updater)
+
+BOT_TOKEN = '7977384112:AAHuuTeQe2S8yz3IbPq7Mt6BVpc8LB5w27w'
+MAGICMIRROR_URL = 'http://localhost:8080/custom-message'
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -24,30 +19,13 @@ async def recado(update: Update, context: ContextTypes.DEFAULT_TYPE):
         message = {"messageHeader": "alguem te deixou um recado: ", "message": ' '.join(context.args)}
 
         try:
-            requests.post(MAGICMIRROR_URL, json=message)        
+            requests.post(MAGICMIRROR_URL, json=message)
             await update.message.reply_text('Sua mensagem foi entrege ao MagicMirror!')
         except requests.RequestException as e:
             await update.message.reply_text(f'Não foi possivel enviar seu recado!: {e}')
-            
-
-        # using file to comunicate with MagicMirror
-        # with open(MESSAGE_FILE, 'w') as file:
-        #     print("INFO: clearing message file.")
-
-        # with open(MESSAGE_FILE, 'a') as file:
-        #     text = ' '.join(context.args)
-        #     await update.message.reply_text(f'Sua mensagem: {text}')
-        #     file.write(text + '\n')
-         
-        # with open(REMOTE_FILE, 'w') as remotefile:
-        #     text = ' '.join(context.args)
-        #     await update.message.reply_text(f'Sua mensagem: {text}')
-        #     x = {'anytime' : text}
-        #     remotefile.write(json.dumps(x))
-
 
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     handlers = {
         'recado': CommandHandler('recado', recado)
