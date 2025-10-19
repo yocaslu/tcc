ab01fb57708bd9483bc97947f1ac480ade77ae6e
lucas <lsantos2003silva@gmail.com>
2025-10-19 20:45:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 import json
-import requests
 import logging
-from pprint import pprint
 from logging import Logger
+from pprint import pprint
+
+import requests
 from telegram import Update
-from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes, Updater)
+from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes,
+                          Updater)
+
+from server import REMOTE_CONTROL_API_KEY
 
 BOT_TOKEN = '7977384112:AAHuuTeQe2S8yz3IbPq7Mt6BVpc8LB5w27w'
-MAGICMIRROR_URL = 'http://localhost:8080/custom-message'
+CUSTOM_MESSAGE_URL = 'http://localhost:8080/custom-message'
+REMOTE_CONTROL_API_KEY = 'aa13c710bab642ca843ef59595d6341b'
+CUSTOM_TEXT_URL = 'http://localhost:8080/api/notification/CUSTOMTEXT_UPDATE'
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+
 async def recado(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.args:
-        message = {"messageHeader": "alguem te deixou um recado: ", "message": ' '.join(context.args)}
+        header = {'Authorization': f"Bearer {REMOTE_CONTROL_API_KEY}", 'Content-type': 'application/json'}
+        json = {"message": " ".join(context.args)}
 
         try:
-            requests.post(MAGICMIRROR_URL, json=message)
-            await update.message.reply_text('Sua mensagem foi entrege ao MagicMirror!')
+            x = requests.post(CUSTOM_TEXT_URL, json=json, headers=header)
+            pprint(f'POST REQUEST RESPONSE: {x.__dict__}')
+            await update.message.reply_text('Seu recado foi enviado ao MagicMirror!')
         except requests.RequestException as e:
-            await update.message.reply_text(f'Não foi possivel enviar seu recado!: {e}')
+            await update.message.reply_text(f'Falha ao enviar seu recado: {e}')
+
+           
+#           USING CUSTOM-MESSAGE
+# async def recado(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     if context.args:
+#         message = {
+#             "messageHeader" : "Novo recado: ",
+#             "message" : ' '.join(context.args)
+#         }
+
+#         try:
+#             requests.post(MAGICMIRROR_URL, json=message)
+#             await update.message.reply_text('Seu recado foi entrege ao MagicMirror!')
+#         except requests.RequestException as e:
+#             await update.message.reply_text(f'Não foi possivel enviar seu recado!: {e}')
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
