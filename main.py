d956f3b3db81e58aad0df59f61570ba6246c29a3
lucas <lsantos2003silva@gmail.com>
2025-10-19 01:15:34 -0300

    using post request to custom-text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from pprint import pprint
 
+import requests
 from telegram import Update
 from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes,
                           Updater)
@@ -8,6 +9,7 @@ from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes,
 TOKEN = '7977384112:AAHuuTeQe2S8yz3IbPq7Mt6BVpc8LB5w27w'
 MESSAGE_FILE = '/tmp/test.txt'
 REMOTE_FILE = '/tmp/remotefile.json'
+MAGICMIRROR_URL = 'http://localhost:8080/custom-message'
 
 import logging
 from logging import Logger
@@ -19,13 +21,23 @@ logging.basicConfig(
 
 async def recado(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.args:
-        with open(MESSAGE_FILE, 'w') as file:
-            print("INFO: clearing message file.")
+        message = {"messageHeader": "alguem te deixou um recado: ", "message": ' '.join(context.args)}
 
-        with open(MESSAGE_FILE, 'a') as file:
-            text = ' '.join(context.args)
-            await update.message.reply_text(f'Sua mensagem: {text}')
-            file.write(text + '\n')
+        try:
+            requests.post(MAGICMIRROR_URL, json=message)        
+            await update.message.reply_text('Sua mensagem foi entrege ao MagicMirror!')
+        except requests.RequestException as e:
+            await update.message.reply_text(f'Não foi possivel enviar seu recado!: {e}')
+            
+
+        # using file to comunicate with MagicMirror
+        # with open(MESSAGE_FILE, 'w') as file:
+        #     print("INFO: clearing message file.")
+
+        # with open(MESSAGE_FILE, 'a') as file:
+        #     text = ' '.join(context.args)
+        #     await update.message.reply_text(f'Sua mensagem: {text}')
+        #     file.write(text + '\n')
          
         # with open(REMOTE_FILE, 'w') as remotefile:
         #     text = ' '.join(context.args)
