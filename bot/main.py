2ce3e70645230590a6f8d87abedb503de12a3e07
lucas <lsantos2003silva@gmail.com>
2025-11-05 20:47:13 -0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,9 @@
 from logging import Logger
 from pprint import pprint
 
-from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from modules.bot import run_bot, APP
-from modules.menus import button_callback, main_menu
+from modules.menus import button_callback, main_menu, handle_message
 
 
 def main():
@@ -11,6 +11,7 @@ def main():
     # endereça as funções responsáveis pela funcionalidade dos commandos
     APP.add_handler(CommandHandler('menu', main_menu))
     APP.add_handler(CallbackQueryHandler(button_callback))
+    APP.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     # inicia o bot
     run_bot()
